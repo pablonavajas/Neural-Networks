6072e501cf46727332e5cbd9879a33ab0180445c
Iurie Tarlev <it119@ic.ac.uk>
2020-02-29 16:55:44 +0000

    delted some unnecessary files (used in testing)

--- LinearLayerTest.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import
-
-def fun(x):
-    return x + 1
-
-class MyTest(unittest.TestCase):
-    def test(self):
-        self.assertEqual(fun(3), 4)
--- example.py
+++
@@ -1,6 +0,0 @@
-import readData
-
-c = readData.Dataset("part2_training_data.csv")
-features, labels = c
-
-print(features.shape)
