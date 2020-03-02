0466da3a786b3eab43ee6e14961632e57e6dc1b6
Iurie Tarlev <it119@ic.ac.uk>
2020-03-02 11:25:29 +0000

    renamed the function of balance_split_into_... to split_into... and removed some unnecessary comments and redundant lines

--- readData.py
+++ readData.py
@@ -31,7 +31,7 @@ class Dataset:
         return L, J
 
 # function to split the data into train, validation and test sets
-def balance_and_split_into_train_valid_test(filename):
+def split_into_train_valid_test(filename):
     # Read in the data
     data = Dataset(filename)
     original_n = data.attributes.shape[0]
@@ -40,37 +40,6 @@ def balance_and_split_into_train_valid_test(filename):
     indices = np.random.permutation(original_n)
     data.attributes = data.attributes[indices]
     data.labels = data.labels[indices]
-    """
-    unique_labels, count = np.unique(data.labels, return_counts=True)
-
-    zeros = np.where(data.labels==0)
-    ones = np.where(data.labels==1)
-
-    zero_att = data.attributes[zeros]
-    one_att = data.attributes[ones]
-
-    zero_lab = data.labels[zeros]
-    one_lab = data.labels[ones]
-
-    print(zero_att.shape)
-    print(zero_lab.shape)
-    print(one_att.shape)
-    print(one_lab.shape)
-
-    zero_att = zero_att[:one_att.shape[0]]
-    zero_lab = zero_lab[:one_att.shape[0]]
-
-    data.attributes = np.vstack((zero_att, one_att))
-    data.labels = np.append(zero_lab, one_lab)
-
-
-    unique_labels, count = np.unique(data.labels, return_counts=True)
-    print(unique_labels)
-   
-    indices = np.random.permutation(data.attributes.shape[0])
-    data.attributes = data.attributes[indices]
-    data.labels = data.labels[indices]
-     """
 
     split_point1 = (data.attributes.shape[0] * 8) // 10
     split_point2 = (data.attributes.shape[0] * 9) // 10
@@ -84,27 +53,6 @@ def balance_and_split_into_train_valid_test(filename):
     test_att = data.attributes[split_point2:]
     test_lab = data.labels[split_point2:]
 
-    unique_labels, count = np.unique(train_lab, return_counts=True)
-    print(unique_labels)
-
-    unique_labels, count = np.unique(valid_lab, return_counts=True)
-    print(unique_labels)
-
-    unique_labels, count = np.unique(test_lab, return_counts=True)
-    print(unique_labels)
-
     return train_att, train_lab, valid_att, valid_lab, test_att, test_lab
 
 
-def main():
-    dataset = balance_and_split_into_train_valid_test("part2_training_data.csv")
-    train_att, train_lab, valid_att, valid_lab, test_att, test_lab = dataset
-    print(train_att.shape)
-    print(train_lab.shape)
-    print(valid_att.shape)
-    print(valid_lab.shape)
-    print(test_att.shape)
-    print(test_lab.shape)
-
-if __name__ == "__main__":
-    main()
